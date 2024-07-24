42eee814c827ec2dc3c13737140afc96d53e89ba
YunJaeHoon <sleepzzz214@naver.com>
2024-07-24 22:55:14 +0900

    FTR: 랭킹 시스템 완성

--- src/component/WaitServer.js
+++ src/component/WaitServer.js
@@ -1,7 +1,9 @@
 import React, { useContext } from 'react';
+import { BarLoader } from "react-spinners";
 
 import ThemeContext from "../context/ThemeContext";
 
+import style from './WaitServerStyle.module.css';
 import colorStyle from '../style/Color.module.css';
 
 function WaitServer() {
@@ -10,12 +12,18 @@ function WaitServer() {
   const { theme } = useContext(ThemeContext);
 
   return (
-    <div
-      className={theme === "LIGHT" ? colorStyle["black-font"] : colorStyle["white-font"]}
-      style={{ textAlign: 'center', fontSize: "6vh", marginTop: "38vh" }}
-    >
-      서버로부터 응답을 기다리는 중입니다.<br />
-      잠시만 기다려주세요.
+    <div id={style["container"]} className={theme === "LIGHT" ? colorStyle["black-font"] : colorStyle["white-font"]}>
+      <div id={style["content"]}>
+        <div id={style["message"]}>
+          서버로부터 응답을 기다리는 중입니다.<br />
+          잠시만 기다려주세요.
+        </div>
+        <BarLoader
+          id={style["load-icon"]}
+          color={theme === "LIGHT" ? "#20201E" : "#FFFFFF"}
+          speedMultiplier={1.3}
+        />
+      </div>
     </div>
   );
 }
