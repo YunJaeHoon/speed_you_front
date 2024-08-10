589cc19505106e35ce8107b609403b0c7a1da3d6
YunJaeHoon <sleepzzz214@naver.com>
2024-08-10 23:50:40 +0900

    FIX: api 요청을 보낼 백엔드 서버 주소 변경

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 import axios from 'axios';
 import style from './index.css';
 
-axios.defaults.baseURL = 'http://localhost:8080';   // axios 기본 경로 설정
+axios.defaults.baseURL = 'http://43.203.237.245/:8080';   // axios 기본 경로 설정
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
