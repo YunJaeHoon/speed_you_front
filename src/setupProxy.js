2abaa02b16c4b94a112fc84be8cbc2566f1534bb
YunJaeHoon <sleepzzz214@naver.com>
2024-08-10 23:55:59 +0900

    FIX: api 요청을 보낼 백엔드 서버 주소 오타 수정

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -6,7 +6,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://43.203.237.245/:8080',	// 서버 URL
+      target: 'http://43.203.237.245:8080',	// 서버 URL
       changeOrigin: true,
     })
   );
